3aa2f8dbb698d3fba7b008950acb26007b9f3706
zantiago el-scrikkidyes <73414733+SANTIAGOVELASCOJOSEMANUEL@users.noreply.github.com>
2021-10-31 15:06:19 -0600

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#by banner
+cp -r banner.sh $PREFIX/etc/bash.bashrc
+mydir=$(rm -rf /data/data/com.termux/files/usr/etc/motd)
+
+echo $mydir
+echo ""
+
+
+echo -e "\e[31m                   W\e[0m"
+echo -e "\e[1;31m                  WWW\e[0m"
+echo -e "\e[1;30m                  WWW\e[0m"
+echo -e "\e[34m                 WWWWW\e[0m"
+echo -e "\e[1;34m           W     WWWWW     W\e[0m"
+echo -e "\e[32m           WWW   WWWWW   WWW\e[0m"
+echo -e "\e[1;32m            WWW  WWWWW  WWW\e[0m"
+echo -e "\e[36m             WWW  WWW  WWW\e[0m"
+echo -e "\e[1;36m              WWW WWW WWW\e[0m"
+echo -e "\e[35m                WWWWWWW\e[0m"
+echo -e "\e[1;35m             WWWW  |  WWWW\e[0m"
+echo -e "\e[33m                   |\e[0m"
+echo -e "\e[1;33m                   |\e[0m"
+echo -e "\e[1;32m     🌹🌹🌹 zantiago El-Scrikkidyes🌹🌹🌹\e[0m"
+echo -e "\e[1;32m 🌹🌹 COMANDOS TERMUX SQL LINUX GOOGLE Y MÁS🌹🌹\e[0m"
+echo""
+PS1='\[\e[1;35m\]➢✦☪NINGÚN SISTEMA ES SEGURO➢✦☪\e[0m
+\[\e[1;32m\]>>'
